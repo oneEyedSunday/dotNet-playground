bf08b3df7d3afeba8da5ce54d65db0869abbf617
Idiakose O. Sunday <idiakosesunday@gmail.com>
2020-04-05 15:36:41 +0100

    List all Prooducts

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoCrafts.Models;
+using ContosoCrafts.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -11,15 +13,18 @@ namespace ContosoCrafts.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public IEnumerable<Product> Products { get; private set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, Products _products)
         {
             _logger = logger;
+            Products = _products.GetProducts();
         }
 
         public void OnGet()
         {
             System.Console.WriteLine("Ok, we are here!!!");
+            System.Console.WriteLine("The number of products are {0}", Products.Count());
         }
     }
 }
---
+++ Services/Products.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using ContosoCrafts.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ContosoCrafts.Services
+{
+    public class Products
+    {
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        public Products(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        private string JsonSrcFileName
+        {
+            get
+            {
+                return Path.Combine(WebHostEnvironment.WebRootPath, "database", "products.json");
+            }
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using StreamReader jsonFileReader = File.OpenText(JsonSrcFileName);
+            return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(), new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+        }
+    }
+}
