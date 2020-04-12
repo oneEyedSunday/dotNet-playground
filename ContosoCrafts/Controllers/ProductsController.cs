59f074c156f9ef82bb3b62f1d3a6ebda0a73558e
Idiakose O. Sunday <idiakosesunday@gmail.com>
2020-04-12 01:42:01 +0100

    Handle 404

--- ContosoCrafts/Controllers/ProductsController.cs
+++ ContosoCrafts/Controllers/ProductsController.cs
@@ -6,6 +6,7 @@ using ContosoCrafts.Models;
 using ContosoCrafts.Services;
 using ContosoCrafts.Controllers.Requests;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,12 +16,14 @@ namespace ContosoCrafts.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        public ProductsController(Products _products)
+        public ProductsController(Products _products, ILogger<ProductsController> logger)
         {
             ProductsService = _products;
+            _logger = logger;
         }
 
         public Products ProductsService { get; }
+        private readonly ILogger<ProductsController> _logger;
 
         // GET: /<controller>/
         [HttpGet]
@@ -31,9 +34,16 @@ namespace ContosoCrafts.Controllers
 
         [HttpGet]
         [Route("{productId}")]
-        public Product GetProduct(string productId)
+        public ActionResult<Product> GetProduct(string productId)
         {
-            return ProductsService.GetProduct(productId);
+            try
+            {
+               return ProductsService.GetProduct(productId);
+            } catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, new { productId = productId });
+                return NotFound($"Product #{productId} not found.\n");
+            }
         }
 
         [HttpPatch]
