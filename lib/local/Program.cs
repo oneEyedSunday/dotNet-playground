7c0240b1eb4a175159ae44cdd21e618dce4e9cab
Idiakose O. Sunday <idiakosesunday@gmail.com>
2020-04-26 02:42:30 +0100

    Locally Test out Library

---
+++ lib/local/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using library.Structures;
+
+namespace local
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> numbers = new Stack<int>();
+            Console.WriteLine("Size of stack is {0}", numbers.Size);
+            numbers.Push(42);
+            numbers.Push(0);
+            Console.WriteLine("Size of stack is {0}, Top is {1}", numbers.Size, numbers.Peek());
+        }
+    }
+}
