394d5f005a02792790dcb2cf2d1eb7cc384e27e7
Idiakose O. Sunday <idiakosesunday@gmail.com>
2020-05-10 12:04:38 +0100

    Implement & Test Sets

---
+++ lib/library.testing/SetTest.cs
@@ -0,0 +1,138 @@
+﻿using Xunit;
+using System;
+
+using library.Structures;
+
+namespace library.testing
+{
+    public class SetTest
+    {
+        [Fact]
+        public void TestToString()
+        {
+
+        }
+
+
+        [Fact]
+        public void TestThatSetStartsEmpty()
+        {
+            Set<string> mySet = new Set<string>();
+            Assert.Equal(0, mySet.Size);
+        }
+
+        [Fact]
+        public void TestAdding()
+        {
+            Set<string> balonDorWinners = new Set<string>();
+
+            balonDorWinners.Add("Kaka");
+            balonDorWinners.Add("Cristiano Ronaldo");
+            balonDorWinners.Add("Lionel Messi");
+            balonDorWinners.Add("Luca Modric");
+            balonDorWinners.Add("Cristiano Ronaldo");
+            balonDorWinners.Add("Lionel Messi");
+
+            Assert.Equal(4, balonDorWinners.Size);
+        }
+
+        [Fact]
+        public void TestRemove()
+        {
+            Set<string> balonDorWinners = new Set<string>();
+
+            balonDorWinners.Add("Kaka");
+            balonDorWinners.Add("Wesley Sneijder");
+            balonDorWinners.Add("Cristiano Ronaldo");
+
+            balonDorWinners.Remove("Wesley Sneijder");
+
+            Assert.Equal(2, balonDorWinners.Size);
+        }
+
+
+        [Fact]
+        public void TestUnion()
+        {
+            Set<string> mySet = new Set<string>();
+
+            mySet.Add("One");
+            mySet.Add("Two");
+            mySet.Add("One");
+
+
+            Set<string> anotherSet = new Set<string>();
+
+            anotherSet.Add("One");
+            anotherSet.Add("Deux");
+            anotherSet.Add("Trois");
+
+
+            Assert.Equal(4, mySet.Union(anotherSet).Size);
+        }
+
+        [Fact]
+        public void TestDifference()
+        {
+            Set<string> mySet = new Set<string>();
+
+            mySet.Add("One");
+            mySet.Add("Two");
+            mySet.Add("One");
+
+
+            Set<string> anotherSet = new Set<string>();
+
+            anotherSet.Add("One");
+            anotherSet.Add("Deux");
+            anotherSet.Add("Trois");
+
+            // S - T
+
+            Assert.Equal(1, mySet.Difference(anotherSet).Size);
+        }
+
+
+        [Fact]
+        public void TestIntersection()
+        {
+            Set<string> mySet = new Set<string>();
+
+            mySet.Add("One");
+            mySet.Add("Two");
+            mySet.Add("One");
+
+
+            Set<string> anotherSet = new Set<string>();
+
+            anotherSet.Add("One");
+            anotherSet.Add("Deux");
+            anotherSet.Add("Trois");
+
+
+            Assert.Equal(1, mySet.Intersection(anotherSet).Size);
+        }
+
+        [Fact]
+        public void TestSubset()
+        {
+            Set<string> balonDorWinners = new Set<string>();
+
+            balonDorWinners.Add("Kaka");
+            balonDorWinners.Add("Cristiano Ronaldo");
+            balonDorWinners.Add("Lionel Messi");
+            balonDorWinners.Add("Luca Modric");
+            balonDorWinners.Add("Cristiano Ronaldo");
+            balonDorWinners.Add("Lionel Messi");
+
+            Assert.Equal(4, balonDorWinners.Size);
+
+            Set<string> somePlayers = new Set<string>();
+
+            //somePlaye
+
+
+            Assert.True(balonDorWinners.Subset(somePlayers));
+        }
+    }
+}
--- lib/library/Structures/Set.cs
+++ lib/library/Structures/Set.cs
@@ -1,10 +1,125 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace library.Structures
 {
     public class Set<T>
     {
-        
+        private List<T> items { get; set; }
+
+        public Set()
+        {
+            items = new List<T>();
+        }
+
+        public int Size
+        {
+            get => items.Count;
+        }
+
+        public bool isEmpty
+        {
+            get => Size == 0;
+        }
+
+        public T[] Values
+        {
+            get => items.ToArray();
+        }
+
+        public void Add(T item)
+        {
+            if (!items.Contains(item))
+            {
+                items.Add(item);
+            }
+        }
+
+        public void Remove(T item)
+        {
+            if (items.Contains(item))
+            {
+                items.Remove(item);
+            }
+        }
+
+
+        /// <summary>
+        /// S U T
+        /// </summary>
+        public Set<T> Union(Set<T> otherSet)
+        {
+            Set<T> unionSet = new Set<T>();
+
+            List<T> allValues = otherSet.Values.ToList();
+            allValues.AddRange(Values);
+
+            foreach (var item in allValues)
+            {
+                unionSet.Add(item);
+            }
+
+            return unionSet;
+        }
+
+        /// <summary>
+        /// S - T
+        /// </summary>
+        public Set<T> Difference(Set<T> otherSet)
+        {
+            Set<T> diffSet = new Set<T>();
+
+            List<T> otherValues = otherSet.Values.ToList<T>();
+
+            foreach (T item in items)
+            {
+                if (!otherValues.Contains(item))
+                {
+                    diffSet.Add(item);
+                }
+            }
+
+            return diffSet;
+        }
+
+
+        /// <summary>
+        /// S N T
+        /// </summary>
+        public Set<T> Intersection(Set<T> otherSet)
+        {
+            Set<T> interSet = new Set<T>();
+
+            List<T> otherValues = otherSet.Values.ToList<T>();
+
+            foreach (T item in otherValues)
+            {
+                if (items.Contains(item))
+                {
+                    interSet.Add(item);
+                }
+            }
+
+            return interSet;
+        }
+
+
+        public bool Subset(Set<T> testSet)
+        {
+            List<T> otherValues = testSet.Values.ToList<T>();
+
+            bool result = true;
+
+            foreach (T item in otherValues)
+            {
+                result &= items.Contains(item);
+            }
+
+            return result;
+        }
+
+        public override string ToString() => $"Set({Size}) {{}}";
+
     }
 }
