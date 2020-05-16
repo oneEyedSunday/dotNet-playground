aa6a4b47cd61808eaea85b014c82bd3f0c6e61f9
Idiakose O. Sunday <idiakosesunday@gmail.com>
2020-05-16 22:13:59 +0100

    Impl Linked lists

---
+++ lib/library.testing/LinkedListTest.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+using library.Structures;
+
+namespace library.testing
+{
+    public class LinkedListTest
+    {
+        [Fact]
+        public void TestFunction()
+        {
+            var myList = new LinkedList<string>();
+
+            myList.Add("A");
+            myList.Add("B");
+            myList.Add("F");
+
+            Assert.Equal(2, myList.Index("F"));
+            myList.Remove("F");
+            Assert.Equal(2, myList.Size);
+            Assert.Equal("B", myList.GetAtIndex(1));
+        }
+    }
+}
