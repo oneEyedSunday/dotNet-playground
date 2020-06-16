df26c379b6b596bed9aaccc42f36a0c93b32e3ce
Idiakose O. Sunday <idiakosesunday@gmail.com>
2020-06-16 20:37:04 +0100

    Ill fated attempt at calling new Greeter.*Client()

---
+++ GrpcDemos/GreeterClients/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+
+namespace GreeterClients
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This switch must be set before creating the GrpcChannel/HttpClient.
+            AppContext.SetSwitch(
+                "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            // var client = new GreeterClient(channel);
+        }
+    }
+}
