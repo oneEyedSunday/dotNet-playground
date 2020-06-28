5b8ea8198cf86f1366fa76f9ad833919a3ecba72
Idiakose O. Sunday <idiakosesunday@gmail.com>
2020-06-28 11:58:53 +0100

    Setup COmpression: Server Level Options

--- GrpcDemos/Walkthrough/StreamServer/Startup.cs
+++ GrpcDemos/Walkthrough/StreamServer/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Compression;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -16,7 +17,11 @@ namespace StreamServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddGrpc();
+            services.AddGrpc(grpcOpts =>
+			{
+                grpcOpts.ResponseCompressionLevel = CompressionLevel.Optimal;
+				grpcOpts.ResponseCompressionAlgorithm = "gzip";
+			});
             services.AddTransient<DatabaseService>();
         }
 
